d4e229c17e348c19705acfefd5a38ac51fc251ac
ripterdust <bryantello2010@hotmail.com>
2023-10-07 01:21:03 -0600

    main, Added bullets and blackNautilus changes

--- Player/Bullets/Bullet.gd
+++ Player/Bullets/Bullet.gd
@@ -7,3 +7,8 @@ func _process(delta):
 	var new_position = position + Vector2(1, 0) * speed * delta
 	
 	position = new_position
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Enemy"):
+		body.die()
