2a9d1fa217923f8cc3eb22f00947174b575cf578
ripterdust <bryantello2010@hotmail.com>
2023-10-07 00:35:44 -0600

    master, Added shoot sound

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,17 +3,30 @@ extends CharacterBody2D
 const SPEED = 300.0
 const bullet_scene := preload('res://Player/Bullets/Bullet.tscn')
 
+@onready var sounds = {
+	"shoot": $Shoot
+}
+
 func shoot(event):
 	if Input.is_action_just_pressed("ui_a") and event is InputEventKey:
 		var bullet := bullet_scene.instantiate()
 		
 		bullet.global_position = global_position + Vector2(40, 0)
 		get_parent().add_child(bullet)
+		sounds["shoot"].play()
 		
 func center_player():
 	var screen_size := get_viewport_rect().size
+
 func _ready():
 	center_player()
 	
 func _input(event):
 	shoot(event)   
+
+func _physics_process(delta):
+	var movement := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	velocity = movement.normalized() * SPEED
+	
+	move_and_slide()
